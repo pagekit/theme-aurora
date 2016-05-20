2922cfc74a42b85813b6d39bf9d4ebb58279aaf4
Danny <danny@yootheme.com>
2016-05-20 17:15:07 +0200

    added top-d

--- views/template.php
+++ views/template.php
@@ -139,6 +139,16 @@
                     </div>
                     <?php endif ?>
 
+                    <?php if ($view->position()->exists('top_d')) : ?>
+                    <div id="tm-top-d" class="tm-top-d uk-block uk-block-default">
+
+                        <section class="uk-grid uk-grid-match" data-uk-grid-margin>
+                            <?= $view->position('top_d', 'position-grid.php') ?>
+                        </section>
+
+                    </div>
+                    <?php endif ?>
+
                     <div id="tm-main" class="tm-main uk-block uk-block-default">
 
                         <div class="uk-grid" data-uk-grid-match data-uk-grid-margin>
